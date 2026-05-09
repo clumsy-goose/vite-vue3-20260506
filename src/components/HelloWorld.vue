544eee7ebf6335e7cb40bf1000d70d0e8972659b
corbinlin <corbinlin@tencent.com>
2026-05-09 15:33:40 +0800

    test env

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -6,6 +6,7 @@ defineProps({
 })
 
 const count = ref(0)
+const testEnv = import.meta.env.VITE_TEST
 </script>
 
 <template>
@@ -16,6 +17,7 @@ const count = ref(0)
     <p>
       Edit
       <code>components/HelloWorld.vue</code> to test HMR
+      环境变量：{{ testEnv }}
     </p>
   </div>
 
