a3f3718620bc7853be43e8aa7773eaa889604ffa
corbinlin <corbinlin@tencent.com>
2026-07-02 17:14:48 +0800

    1

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), ['VITE_', 'TEST', 'NAME', 'ALL'])
+  return {
+    plugins: [vue()],
+    define: {
+      'import.meta.env.TEST': JSON.stringify(env.TEST),
+      'import.meta.env.NAME': JSON.stringify(env.NAME),
+      'import.meta.env.NAME': JSON.stringify(env.ALL),
+    },
+  }
 })
