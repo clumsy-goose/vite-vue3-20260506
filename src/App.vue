c924ce3666b4eec35bb2fb98bb65d7c83fe351b4
corbinlin <corbinlin@tencent.com>
2026-07-07 14:46:07 +0800

    优化

--- src/App.vue
+++ src/App.vue
@@ -15,7 +15,7 @@ import HelloWorld from './components/HelloWorld.vue'
     <a href="/helloworld-cloud">Cloud 函数</a>
     <a href="/helloworld-edge">Edge 函数</a>
   </nav>
-  <HelloWorld msg="这是生产环境" />
+  <HelloWorld msg="这是测试环境" />
 </template>
 
 <style scoped>
